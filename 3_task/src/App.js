0e3e0547423671e9fb96ce0d498c9049e705a1d9
Artem Zagarov <artem.zagarov@gmail.com>
2020-10-30 09:54:12 +0200

    added likes info

--- 3_task/src/App.js
+++ 3_task/src/App.js
@@ -1,12 +1,20 @@
 import './App.css';
- import React from 'react';
- 
- const App = () => {
-   return (
-     <div>
-       
-     </div>
-   );
- };
- 
- export default App;
+import React, { Component } from 'react';
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state ={
+      likes: 0
+    }
+  }
+  render() {
+    return (
+      <div>
+        <h1>Total likes: {this.state.likes}</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
