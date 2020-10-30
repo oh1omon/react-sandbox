cb6b835f76e6e9dbec3be71d5b32c07f2270216d
Artem Zagarov <artem.zagarov@gmail.com>
2020-10-30 10:20:57 +0200

    working buttons

--- 3_task/src/App.js
+++ 3_task/src/App.js
@@ -7,11 +7,38 @@ class App extends Component {
     this.state ={
       likes: 0
     }
+    this.handleAdding = this.handleAdding.bind(this);
+    this.handleDecreasing = this.handleDecreasing.bind(this);
+    this.handleResetting = this.handleResetting.bind(this);
   }
+
+  handleAdding() {
+    this.setState((state) => {
+      return {likes: state.likes += 1}
+    })
+  };
+
+  handleDecreasing() {
+    if(this.state.likes > 0) {
+      this.setState((state) => {
+        return {likes: state.likes -= 1}
+      })
+    }
+  }
+
+  handleResetting() {
+    this.setState(() => {
+      return {likes: 0}
+    })
+  }
+
   render() {
     return (
       <div>
         <h1>Total likes: {this.state.likes}</h1>
+        <button onClick={this.handleAdding}>Add Like</button>
+        <button onClick={this.handleDecreasing}>Delete Like</button>
+        <button onClick={this.handleResetting}>Reset Likes</button>
       </div>
     );
   }
